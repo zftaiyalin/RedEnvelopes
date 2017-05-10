d303d7148c0d4aabba939e2147057376ecdc6713
曾富田 <zftaife@163.com>
2017-05-10 17:38:19 +0800

    Initial Commit

---
+++ RedEnvelopes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedEnvelopes
+//
+//  Created by 曾富田 on 2017/5/10.
+//  Copyright © 2017年 曾富田. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
